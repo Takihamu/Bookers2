1cc0fe25d7470e848e9b6e1f1f35a01bd5b6d102
Takihamu <yukiko0802black@gmail.com>
2021-11-02 12:57:08 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   post 'follow/:id' => 'relationships#follow', as: 'follow'
   post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow'
   resources :users, only: [:index, :create,:show,:edit,:update,:edit, :destroy]do
-    resources :relationships, only: [:create, :destroy]
+    resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
